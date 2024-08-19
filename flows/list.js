f57a9be6f97926b8d21d82e28b2310564c1fda1c
Hieu Nguyen <hieu@newlogic.com>
2024-08-19 10:38:12 +0700

    log chapter, img progress

--- flows/list.js
+++ flows/list.js
@@ -6,14 +6,17 @@ module.exports = (chaplistScraper, imgScraper) =>
             if (cachedChaplists[name] === undefined) {
                 cachedChaplists[name] = await chaplistScraper(name)
             }
-            if (chapter < 1 || chapter > cachedChaplists[name].length) {
+
+            const totalChapters = cachedChaplists[name].length
+            if (chapter < 1 || chapter > totalChapters) {
+                console.log(`${name} has ${totalChapters} chapters`)
                 return
             }
 
             const imgs = await imgScraper(cachedChaplists[name], chapter)
 
             const download = async (url, page) => {
-                console.log(`Downloading ${name} - chap ${chapter} - img ${page}...`)
+                console.log(`Downloading ${name} - chap ${chapter}/${totalChapters} - img ${page}/${imgs.length}...`)
                 const response = await downloader(url)
                 writter(name, chapter, page)(response)
             }
